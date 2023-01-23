2e6d290294fddc656bf584a1014bcb06a9249c40
suvangipaul <suvangipaul8@gmail.com>
2023-01-23 20:55:31 +0530

    Register Modified

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
 import { registerRoute } from "../utils/APIRoutes";
 
-function Register() {
+export default function Register() {
   const navigate = useNavigate();
-  const [values, setValues] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
   const toastOptions = {
     position: "bottom-right",
     autoClose: 8000,
@@ -22,65 +16,81 @@ function Register() {
     draggable: true,
     theme: "dark",
   };
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (handleValidation()) {
-      const { password, username, email } = values;
-      axios
-        .post(registerRoute, {
-          username,
-          email,
-          password,
-        })
-        .then((data) => {
-          if (data.status === false) {
-            toast.error(data.msg, toastOptions);
-            console.log("f");
-          } else if (data.status === true) {
-            localStorage.setItem("chat-app", JSON.stringify(data.user));
-            console.log("s");
-            navigate("/");
-          }
-        });
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
     }
+  }, []);
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
 
   const handleValidation = () => {
     const { password, confirmPassword, username, email } = values;
     if (password !== confirmPassword) {
       toast.error(
-        "password and confirm password should be same.",
+        "Password and confirm password should be same.",
         toastOptions
       );
       return false;
     } else if (username.length < 3) {
-      toast.error("Username should be greater than 3 characters", toastOptions);
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
       return false;
     } else if (password.length < 8) {
       toast.error(
-        "Password should be equal or greater than 8 characters",
+        "Password should be equal or greater than 8 characters.",
         toastOptions
       );
       return false;
     } else if (email === "") {
-      toast.error("email is required", toastOptions);
+      toast.error("Email is required.", toastOptions);
       return false;
     }
+
     return true;
   };
 
-  const handleChange = (event) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-    console.log(values);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+        navigate("/");
+      }
+    }
   };
+
   return (
     <>
       <FormContainer>
-        <form onSubmit={(event) => handleSubmit(event)}>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
-            <img src={Logo} alt="Logo" />
-            <h1>Chatty</h1>
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
           </div>
           <input
             type="text"
@@ -88,7 +98,6 @@ function Register() {
             name="username"
             onChange={(e) => handleChange(e)}
           />
-
           <input
             type="email"
             placeholder="Email"
@@ -109,7 +118,7 @@ function Register() {
           />
           <button type="submit">Create User</button>
           <span>
-            Already Have An Account ? <Link to="/login">Login.</Link>
+            Already have an account ? <Link to="/login">Login.</Link>
           </span>
         </form>
       </FormContainer>
@@ -147,44 +156,41 @@ const FormContainer = styled.div`
     background-color: #00000076;
     border-radius: 2rem;
     padding: 3rem 5rem;
-    input {
-      background-color: transparent;
-      padding: 1rem;
-      border: 0.1rem solid #4e0eff;
-      border-radius: 0.4rem;
-      color: white;
-      width: 100%;
-      font-size: 1rem;
-      &:focus {
-        border: 0.1rem solid #997af0;
-        outline: none;
-      }
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
     }
-    button {
-      background-color: #997af0;
-      color: white;
-      padding: 1rem 2rem;
-      border: none;
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
       font-weight: bold;
-      cursor: pointer;
-      border-radius: 0.4rem;
-      font-size: 1rem;
-      text-transform: uppercase;
-      transition: 0.5s ease-in-out;
-      &:hover {
-        background-color: #4e0eff;
-      }
-    }
-    span {
-      color: white;
-      text-transform: uppercase;
-      a {
-        color: #4e0eff;
-        text-decoration: none;
-        font-weight: bold;
-      }
     }
   }
 `;
-
-export default Register;
