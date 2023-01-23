26fc80286058e03625c4b319f7c05fc8b467379a
suvangipaul <suvangipaul8@gmail.com>
2023-01-23 20:44:36 +0530

    Api Changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,5 +3,7 @@ const { register } = require("../controllers/userController");
 const router = require("express").Router();
 
 router.post("/register", register);
+router.post("/login", login);
+
 
 module.exports = router;
